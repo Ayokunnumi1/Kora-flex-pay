7e889aaa177b16e84c12d35079880e6db36ad3a2
Al-Amin <alaminkhanshakil@gmail.com>
2024-09-23 01:41:54 +0600

    Redirect after a successful payout

--- app/controllers/payouts_controller.rb
+++ app/controllers/payouts_controller.rb
@@ -1,53 +1,16 @@
 class PayoutsController < ApplicationController
   def new
-    @payout = Payout.new
+    @payout = current_user.payouts.new
   end
 
   def create
     @payout = current_user.payouts.new(payout_params)
 
-    body = {
-      reference: @payout.reference,
-      destination: {
-        type: 'bank_account',
-        amount: @payout.amount.to_s,
-        currency: @payout.currency,
-        narration: @payout.narration,
-        bank_account: {
-          bank: @payout.bank_code,
-          account: @payout.account_number
-        },
-        customer: {
-          name: @payout.customer_name,
-          email: @payout.customer_email
-        }
-      }
-    }
-
-    response = HTTParty.post(
-      'https://api.korapay.com/merchant/api/v1/transactions/disburse',
-      headers: {
-        "Authorization" => "Bearer #{current_user.kora_api_sk}",
-        'Content-Type' => 'application/json'
-      },
-      body: body.to_json
-    )
-
-    parsed_response = response.parsed_response
-
-    if response.success? && parsed_response['status'] == 'success'
-      @payout.status = 'processing'
-      if @payout.save
-        respond_to do |format|
-          format.html { redirect_to users_path, notice: 'Payout initiated successfully.' }
-          format.turbo_stream { redirect_to users_path, notice: 'Payout initiated successfully.' }
-        end
-      else
-        flash[:alert] = 'Failed to save payout in the database.'
-        render :new
-      end
+    if create_payout_api(@payout)
+      @payout.save
+      redirect_to users_path, notice: 'Payout was successfully created.'
     else
-      flash[:alert] = "Failed to initiate payout: #{parsed_response['message']}"
+      flash.now[:alert] = 'Failed to create payout via API'
       render :new
     end
   end
@@ -56,5 +19,39 @@ class PayoutsController < ApplicationController
 
   def payout_params
     params.require(:payout).permit(:reference, :amount, :currency, :bank_code, :account_number, :narration, :customer_name, :customer_email)
+  end
+
+  def create_payout_api(payout)
+    url = 'https://api.korapay.com/merchant/api/v1/transactions/disburse'
+    headers = {
+      'Authorization' => "Bearer #{current_user.kora_api_sk}",
+      'Content-Type' => 'application/json'
+    }
+
+    payload = {
+      reference: payout.reference,
+      destination: {
+        type: 'bank_account',
+        amount: payout.amount.to_s,
+        currency: payout.currency,
+        narration: payout.narration,
+        bank_account: {
+          bank: payout.bank_code,
+          account: payout.account_number
+        },
+        customer: {
+          name: payout.customer_name,
+          email: payout.customer_email
+        }
+      }
+    }.to_json
+
+    response = HTTParty.post(url, headers: headers, body: payload)
+
+    if response.code === 200 && response.parsed_response['status']
+      true
+    else
+      false
+    end
   end
 end
