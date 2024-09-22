3b209ad6f60e0730e82250c3fd64fa264d31417a
Al-Amin <alaminkhanshakil@gmail.com>
2024-09-23 02:47:32 +0600

    Create list of payouts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
     @user = current_user
     @user_balance = @user.fetch_balance
     @bank_transfers = @user.bank_transfers
+    @payouts = @user.payouts
   end
 end
