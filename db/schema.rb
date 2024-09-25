967a5041ed92eb06601ecaf06c2ed2e4a19455be
Ayokunnumi1 <omololuayk@gmail.com>
2024-09-25 22:52:48 +0100

    Update the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_21_190903) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_25_053327) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_21_190903) do
     t.string "customer_name"
     t.string "customer_email"
     t.index ["user_id"], name: "index_bank_transfers_on_user_id"
+  end
+
+  create_table "mobile_money_transactions", force: :cascade do |t|
+    t.decimal "amount"
+    t.string "currency"
+    t.string "customer_name"
+    t.string "customer_email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "payouts", force: :cascade do |t|
