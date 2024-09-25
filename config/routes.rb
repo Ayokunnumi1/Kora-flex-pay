8049e1319fd935ffe13d3ac3d68c1d8ef190916c
Ayokunnumi1 <omololuayk@gmail.com>
2024-09-25 23:00:03 +0100

    Update the route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
+  get 'mobile_money_transactions/create'
+  # get 'mobile_money/create'
   root 'users#index'
+  get '/payment_success', to: 'payments#success', as: 'payment_success'
   resources :users, only: [:index]
+  resources :mobile_money_transactions, only: [:new, :create, :index]
+  
   resources :bank_transfers do
     collection do
       post :create
     end
   end
+
   resources :payouts do
     collection do
       post :create
