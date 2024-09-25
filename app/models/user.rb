8eb2d6f585187f1b76decef2b8a1f7c1d22c91ea
Al-Amin <alaminkhanshakil@gmail.com>
2024-09-26 00:20:59 +0600

    Install devise to authenticate users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   before_create :generate_unique_identifier
 
   validates :unique_identifier, uniqueness: true
