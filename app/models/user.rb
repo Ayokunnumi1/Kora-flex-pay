b4eeb3bf0ea6825e4acfacd33ab00ef9ea97cef2
Ayokunnumi1 <omololuayk@gmail.com>
2024-09-26 02:16:59 +0100

    Fix linter error

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
   base_uri 'https://api.korapay.com/merchant/api/v1'
 
   def fetch_balance
-    response = self.class.get('/balances', headers: { "Authorization" => "Bearer #{kora_api_sk}" })
+    response = self.class.get('/balances', headers: { 'Authorization' => "Bearer #{kora_api_sk}" })
 
-    puts response.parsed_response  # To debug the response
+    puts response.parsed_response # To debug the response
 
     if response.success?
       data = response.parsed_response
@@ -20,18 +20,19 @@ class User < ApplicationRecord
     else
       { error: response.message }
     end
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :validatable
 
-  before_create :generate_unique_identifier
+    before_create :generate_unique_identifier
 
-  validates :unique_identifier, uniqueness: true
+    validates :unique_identifier, uniqueness: true
 
-  private
+    private
 
-  def generate_unique_identifier
-    self.unique_identifier = SecureRandom.uuid
+    def generate_unique_identifier
+      self.unique_identifier = SecureRandom.uuid
+    end
   end
 end
