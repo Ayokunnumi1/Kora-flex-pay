d12b4c88770f6316cabbe24d9752697c4cfd10e0
Ayokunnumi1 <omololuayk@gmail.com>
2024-09-26 04:08:56 +0100

    Remove balances from this user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,38 +1,21 @@
 class User < ApplicationRecord
-  include HTTParty
+  # include HTTParty
   has_many :bank_transfers
   has_many :payouts
   has_many :mobile_money_transactions
 
-  base_uri 'https://api.korapay.com/merchant/api/v1'
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
-  def fetch_balance
-    response = self.class.get('/balances', headers: { 'Authorization' => "Bearer #{kora_api_sk}" })
+  before_create :generate_unique_identifier
 
-    puts response.parsed_response # To debug the response
+  validates :unique_identifier, uniqueness: true
 
-    if response.success?
-      data = response.parsed_response
-      {
-        pending_balance: data.dig('data', 'NGN', 'pending_balance'),
-        available_balance: data.dig('data', 'NGN', 'available_balance')
-      }
-    else
-      { error: response.message }
-    end
-    # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-    devise :database_authenticatable, :registerable,
-           :recoverable, :rememberable, :validatable
+  private
 
-    before_create :generate_unique_identifier
-
-    validates :unique_identifier, uniqueness: true
-
-    private
-
-    def generate_unique_identifier
-      self.unique_identifier = SecureRandom.uuid
-    end
+  def generate_unique_identifier
+    self.unique_identifier = SecureRandom.uuid
   end
 end
