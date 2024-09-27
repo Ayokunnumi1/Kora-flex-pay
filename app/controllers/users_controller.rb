07e9af4322c160232bcf826a2dba0b351c622034
Ayokunnumi1 <omololuayk@gmail.com>
2024-09-27 05:45:35 +0100

    Add total customers to user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,7 @@
 class UsersController < ApplicationController
   def index
     @user = current_user
-    # @user_balance = @user.fetch_balance
-    # @bank_transfers = @user.bank_transfers
-    # @payouts = @user.payouts
     @mobile_money_transaction = @user.mobile_money_transactions
+    @total_customers = MobileMoneyTransaction.select(:customer_name).distinct.count
   end
 end
