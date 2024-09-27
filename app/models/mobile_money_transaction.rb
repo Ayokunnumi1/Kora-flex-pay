f0c0fedae06e7337273e0e9d2085083c67d5765c
Al-Amin <alaminkhanshakil@gmail.com>
2024-09-27 21:04:27 +0600

    Update users available balance

--- app/models/mobile_money_transaction.rb
+++ app/models/mobile_money_transaction.rb
@@ -1,3 +1,12 @@
 class MobileMoneyTransaction < ApplicationRecord
   belongs_to :user
+
+  after_create :update_user_balance
+
+  private
+
+  def update_user_balance
+    new_balance = amount * 0.9619
+    user.update(available_balance: user.available_balance + new_balance)
+  end
 end
