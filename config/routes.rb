8055a466f206e6f0de9344308ae765b6e2a012b8
Al-Amin <alaminkhanshakil@gmail.com>
2024-09-28 15:39:17 +0600

    Fix status

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :admins, skip: [:registrations]
   get 'mobile_money_transactions/create'
-  # get 'mobile_money/create'
   devise_for :users
   root 'users#index'
   get 'admins/index'
@@ -10,12 +9,6 @@ Rails.application.routes.draw do
   get '/payment_success', to: 'payments#success', as: 'payment_success'
   resources :users, only: [:index, :show]
   resources :mobile_money_transactions, only: [:new, :create, :index]
-
-  resources :bank_transfers do
-    collection do
-      post :create
-    end
-  end
 
   resources :payouts do
     member do
